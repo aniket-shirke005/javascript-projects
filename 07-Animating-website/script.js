1580663b5e7f1b77fcacea98b32ee52a7355aa5d
aniket <aniket shirke>
2022-02-03 17:14:54 +0530

    using aos,loading js files

---
+++ 07-Animating-website/script.js
@@ -0,0 +1,7 @@
+AOS.init({
+  delay: 300, // values from 0 to 3000, with step 50ms
+  duration: 1500, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: false,
+  mirror: false,
+});
